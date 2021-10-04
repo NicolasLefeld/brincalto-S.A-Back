118ab0541ec535fed7cdad4015a09f6f64294f8b
Nicolas Lefeld <lefeldnicolas@gmail.com>
2021-10-04 14:59:54 -0300

    Connection modified to mongo db atlas

--- src/config/dbConection.js
+++ src/config/dbConection.js
@@ -1,29 +1,31 @@
 const mongoose = require("mongoose");
 
-const { MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOSTNAME, MONGO_DB_NAME, MONGO_PORT } =
-  process.env;
+
+const {
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_HOSTNAME,
+  MONGO_DB_NAME,
+} = process.env;
 mongoose.Promise = global.Promise;
 mongoose.set("debug", false); // Will add the Mongo actions on the console
-mongoose.set("useFindAndModify", false);
 
 const connectToDb = async () => {
-
-  const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB_NAME}?authSource=admin`;
-
+  const uri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DB_NAME}?retryWrites=true&w=majority`;
+  
   const options = {
-    poolSize: 5,
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   };
 
   try {
-    const Db = await mongoose.connect(url, options);
-  
+    const Db = await mongoose.connect(uri, options);
+
     if (Db.connections[0].readyState === 1) {
       console.log("🌴 Connected to MongoDB 🌴");
     }
   } catch (error) {
-    console.log('💥 Error at DB connection 💥', url, options, error);
+    console.log("💥 Error at DB connection 💥", uri, options, error);
   }
 };
 
