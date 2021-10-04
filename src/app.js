ac52ce191fe9b19341cefff18b1a7b866cdab4b5
Nicolas Lefeld <lefeldnicolas@gmail.com>
2021-10-04 15:59:36 -0300

    Added cors

--- src/app.js
+++ src/app.js
@@ -4,7 +4,18 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors())
+var whitelist = ['https://brincalto-front.herokuapp.com/']
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
