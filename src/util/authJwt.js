33e94db6637f5888de05c287529986e6872db28f
Nicolas Lefeld <lefeldnicolas@gmail.com>
2021-11-11 12:39:56 -0300

    fix jwt

--- src/util/authJwt.js
+++ src/util/authJwt.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 verifyToken = (req, res, next) => {
   if (process.env.NODE_ENV === "production" && req.url.search("/auth") === -1) {
-    let token = req.headers["x-access-token"];
+    let token = req.headers["Authorization"];
 
     if (!token) {
       return res.status(403).send({ message: "No token provided!" });
